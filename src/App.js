e6327dc6a1e2fad3d58bd9e29a787eb6e385d8ce
unknown <vialisthalady@gmail.com>
2021-07-26 20:34:41 +0700

    add modal product list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React from "react";
 import Data from "../src/data.json";
 import CardProduct from "./component/card";
-import { Row, Col } from "reactstrap";
+import {
+    Row,
+    Col,
+    Modal,
+    ModalBody,
+    Button,
+    ModalFooter,
+    ModalHeader,
+} from "reactstrap";
 import styled from "styled-components";
 import "./App.css";
 
 const Style = styled.div`
     width: 980px;
     margin: auto;
-    Col {
-        max-height: 300px;
+    .btn-product {
+        background: transparent;
+        border: unset;
     }
 `;
 const App = () => {
-    console.log(Data);
-    const [showData, setShowData] = React.useState(true);
-    const [thisData, setThisData] = React.useState([]);
-    React.useEffect(() => {
-        if (showData) {
-            console.log("enter");
-            console.log(thisData);
-            setThisData(Data.content);
-            setShowData(false);
-        }
-    }, [showData]);
+    const [product, setProduct] = React.useState({});
+    const [isShow, setShow] = React.useState(false);
+
+    const handleGetProduct = (item) => {
+        console.log(item);
+        setShow(true);
+        setProduct(item);
+    };
+    // React.useEffect(() => {
+    //     if (showData) {
+    //         console.log("enter");
+    //         console.log(thisData);
+    //         setThisData(Data.content);
+    //         setShowData(false);
+    //     }
+    // }, [showData]);
     return (
         <div className="App">
             <Style>
@@ -32,13 +46,49 @@ const App = () => {
                         {Data.content.map((item, key) => {
                             return (
                                 <Col xl="3" key={key}>
-                                    <div>
+                                    <button
+                                        className="btn-product"
+                                        onClick={() => handleGetProduct(item)}
+                                    >
                                         <CardProduct item={item} />
-                                    </div>
+                                    </button>
                                 </Col>
                             );
                         })}
                     </Row>
+                    <div>
+                        <Modal isOpen={isShow} toggle={() => setShow(false)}>
+                            <ModalHeader toggle={() => setShow(false)}>
+                                {product.name}
+                            </ModalHeader>
+                            <ModalBody>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint
+                                occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum.
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button
+                                    color="primary"
+                                    onClick={() => setShow(false)}
+                                >
+                                    Do Something
+                                </Button>{" "}
+                                <Button
+                                    color="secondary"
+                                    onClick={() => setShow(false)}
+                                >
+                                    Cancel
+                                </Button>
+                            </ModalFooter>
+                        </Modal>
+                    </div>
                 </section>
             </Style>
         </div>
--- src/component/card/index.js
+++ src/component/card/index.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import Data from "../../data.json";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
-import image from "../../asset/images.jpg";
 import styled from "styled-components";
 
 const Style = styled.div`
     padding: 15px;
-    Card {
-        width: 700px;
-        padding: 15px;
+    .product {
+        &-name {
+            margin-bottom: 4px;
+            font-size: 0.86rem;
+            color: rgba(49, 53, 59, 0.96);
+            max-width: 100%;
+            max-height: 100%;
+            text-overflow: ellipsis;
+            word-break: break-all;
+            overflow: hidden;
+            white-space: pre-wrap;
+            line-height: 1.5;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+        }
+        &-desc {
+            margin-bottom: 4px;
+            font-size: 0.66rem;
+            color: rgba(49, 53, 59, 0.96);
+            max-width: 100%;
+            max-height: 100%;
+            text-overflow: ellipsis;
+            word-break: break-all;
+            overflow: hidden;
+            white-space: pre-wrap;
+            line-height: 1.5;
+            display: -webkit-box;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;
+        }
+        &-image {
+            width: 100%;
+            display: block;
+            height: 181px;
+            object-fit: contain;
+        }
     }
 `;
 
@@ -16,15 +50,21 @@ const CardProduct = ({ item }) => {
         <Style>
             <Card>
                 <CardImg
+                    className="product-image"
                     top
                     width="100%"
                     src={item?.product_image[0]?.img_url}
                     alt="Card image cap"
                 />
                 <CardBody>
-                    <CardTitle tag="h5">{item.name}</CardTitle>
+                    <CardTitle tag="h5" className="product-name">
+                        {item.name}
+                    </CardTitle>
                     <CardText>{item.price}</CardText>
-                    <CardText>{item.description}</CardText>
+                    <div
+                        className="product-desc"
+                        dangerouslySetInnerHTML={{ __html: item.description }}
+                    ></div>
                 </CardBody>
             </Card>
         </Style>
